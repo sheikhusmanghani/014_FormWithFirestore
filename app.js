05753139b0b6d14ede54d0d05291a769f75dad3b
Hafiz-Usman-Ghani-123 <hafizusmanghani22322@gmail.com>
2024-09-01 20:09:56 +0500

    firestore init ok

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.1/firebase-app.js";
 import {
-  initializeApp,
+  getFirestore,
   collection,
   addDoc,
-} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firstore.js";
+} from "https://www.gstatic.com/firebasejs/10.13.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCUtz5awB6XDshCTzKUw_PWnc8ePw0pjhw",
@@ -17,18 +18,27 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
-// ==============================================================================================
+// ==================================================================== variabls =======================
+const btn = document.getElementById("btn");
+const name = document.getElementById("name");
+const email = document.getElementById("email");
+const number = document.getElementById("number");
+const age = document.getElementById("age");
 
-async function setUserData(){
-    
-try {
-  const docRef = await addDoc(collection(db, "users"), {
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815,
-  });
-  console.log("Document written with ID: ", docRef.id);
-} catch (e) {
-  console.error("Error adding document: ", e);
-}
+console.log(btn, name, email, number, age);
+
+// ========================================================================  ===================
+
+
+async function setUserData() {
+  try {
+    const docRef = await addDoc(collection(db, "users"), {
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
 }
